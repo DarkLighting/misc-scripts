3bdd7d248afe86e069fdc4df6787ebb25263eb18
Ailton Caetano <ailtoncaetanos@gmail.com>
2016-03-22 22:30:44 +0000

    added a script to check for zone transfers in dns servers

---
+++ ns_transfer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python2
+
+import sys
+import dns.resolver
+
+def print_help():
+    print("\nSyntax: "+sys.argv[0]+" <domain> [record type]")
+    print("\n\tThis tool checks for dns servers that allow zone transfers\n")
+    exit()
+
+def domain_val(_domain):
+    if(domain is not None):
+        if(domain.count('.') < 1):
+            print("\n\tInvalid domain.")
+            print_help()
+
+def print_soa(rec):
+    for entry in rec:
+        print("\t"+str(entry.mname)[:-1])
+
+def get_records(records):
+    for rec in records:
+        print("\t"+rec)
+
+if(len(sys.argv) == 1 or len(sys.argv) > 3):
+    print_help()
+
+domain = sys.argv[1]
+domain_val(domain)
+nameservers = list()
+
+try:
+    soa = dns.resolver.query(domain, 'SOA')
+    print("\nStart of Authority - "+domain)
+    print_soa(soa)
+    for ns in dns.resolver.query(domain, 'NS'):
+        nameservers.append(ns.to_text()[:-1])
+    print("\nList of nameservers - "+domain)
+    nameservers.sort()
+    get_records(nameservers)
+    print("\nTrying to make zone transfers from nameservers...")
+    for ns_server in nameservers:
+        xfr = dns.query.xfr(ns_server, domain)
+        try:
+            answer = next(xfr)
+            if(answer is not None):
+                print("*** Server "+ns_server+" allows zone transfers! ***")
+        except:
+            print("\tNameserver "+ns_server+" doesn\'t allow zone transfers")
+    print("\n")    
+
+except:
+    print("\n")
+    print(sys.exc_info())
