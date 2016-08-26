407e9ae52acd0fc6290f9f1b8c330a6dde1e1022
Ailton Caetano <ailtoncaetanos@gmail.com>
2016-08-26 19:46:54 -0300

    Minor update involving timeout

--- ns_transfer.py
+++ ns_transfer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 
+import time
 import sys
 import dns.resolver
 
@@ -30,10 +31,13 @@ domain_val(domain)
 nameservers = list()
 
 try:
-    soa = dns.resolver.query(domain, 'SOA')
+    resolver = dns.resolver.Resolver();
+    resolver.timeout = 10;
+    resolver.lifetime = 10;
+    soa = resolver.query(domain, 'SOA')
     print("\nStart of Authority - "+domain)
     print_soa(soa)
-    for ns in dns.resolver.query(domain, 'NS'):
+    for ns in resolver.query(domain, 'NS'):
         nameservers.append(ns.to_text()[:-1])
     print("\nList of nameservers - "+domain)
     nameservers.sort()
@@ -44,7 +48,10 @@ try:
         try:
             answer = next(xfr)
             if(answer is not None):
+                print 'none'
                 print("*** Server "+ns_server+" allows zone transfers! ***")
+        except dns.resolver.Timeout:
+            print("\tRequest timed out contacting server "+ns_server)
         except:
             print("\tNameserver "+ns_server+" doesn\'t allow zone transfers")
     print("\n")    
